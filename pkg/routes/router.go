4dac10f91dd85e14525180b6e370b9702ceebcef
ashi5lab <ashiqpkodiyathur@gmail.com>
2020-04-30 00:51:45 +0530

    Bug Fixes

--- pkg/routes/router.go
+++ pkg/routes/router.go
@@ -6,27 +6,27 @@ import (
 	"net/http"
 
 	"github.com/ashi5lab/EduLab/pkg/handlers"
+	"github.com/ashi5lab/EduLab/pkg/middlewares"
 	"github.com/gorilla/mux"
 )
 
+//Server struct
 type Server struct {
 	Handler handlers.Server
 	Router  *mux.Router
 }
 
+//InitializeRoutes method
 func (server *Server) InitializeRoutes() {
 	server.Router = mux.NewRouter()
 
-	// Login Route
-	// server.Router.HandleFunc("/login", server.Handler.Login).Methods("POST")
-
-	// Login Route
-	server.AddLoginRouters()
+	server.Router.HandleFunc("/login", middlewares.SetMiddlewareAuthentication(seserver.Handler.Login)).Methods("POST")
 	// User Route
 	server.AddUserRouters()
 
 }
 
+//Run function
 func (server *Server) Run(addr string) {
 	fmt.Println("Listening to port 8080")
 	log.Fatal(http.ListenAndServe(addr, server.Router))
