f7bfc8e53cd0f5859971327d33edd045930247b5
sareenanedungat <sareena96@gmail.com>
2020-05-01 06:14:15 +0530

    Corrected RoleId issue

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -16,7 +16,7 @@ type User struct {
 	UserID    uint32    `gorm:"primary_key;auto_increment" json:"id"`
 	UserName  string    `gorm:"size:40;not null;unique" json:"username"`
 	Email     string    `gorm:"size:50;not null;unique" json:"email"`
-	RoleID    int       `gorm:"not null;" json:"role"`
+	RoleID    int       `gorm:"not null;" json:"roleid"`
 	Password  string    `gorm:"size:100;not null;" json:"_"`
 	IsDeleted bool      `gorm:"default:false" json:"_"`
 	CreatedBy int       `json:"_"`
