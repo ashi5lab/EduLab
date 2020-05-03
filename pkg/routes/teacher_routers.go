5103bd690c2e778641420bd876068b895dac8fc2
ashi5lab <ashiqpkodiyathur@gmail.com>
2020-05-03 19:30:41 +0530

    teacher route changed to `teacher/`

--- pkg/routes/teacher_routers.go
+++ pkg/routes/teacher_routers.go
@@ -4,10 +4,10 @@ import "github.com/ashi5lab/EduLab/pkg/middlewares"
 
 //AddTeacherRouters function
 func (server *Server) AddTeacherRouters() {
-	server.Router.HandleFunc("/teacher", middlewares.SetMiddlewareJSON(server.Handler.CreateTeacher)).Methods("POST")
-	server.Router.HandleFunc("/teacher", middlewares.SetMiddlewareJSON(server.Handler.GetAllTeacher)).Methods("GET")
-	server.Router.HandleFunc("/teacher/{id}", middlewares.SetMiddlewareJSON(server.Handler.GetTeacher)).Methods("GET")
-	server.Router.HandleFunc("/teacher/{id}", middlewares.SetMiddlewareJSON(server.Handler.UpdateTeacher)).Methods("PUT")
+	server.Router.HandleFunc("/teachers", middlewares.SetMiddlewareJSON(server.Handler.CreateTeacher)).Methods("POST")
+	server.Router.HandleFunc("/teachers", middlewares.SetMiddlewareJSON(server.Handler.GetAllTeacher)).Methods("GET")
+	server.Router.HandleFunc("/teachers/{id}", middlewares.SetMiddlewareJSON(server.Handler.GetTeacher)).Methods("GET")
+	server.Router.HandleFunc("/teachers/{id}", middlewares.SetMiddlewareJSON(server.Handler.UpdateTeacher)).Methods("PUT")
 	// server.Router.HandleFunc("/teacher/{id}", middlewares.SetMiddlewareJSON(server.Handler.DeleteTeacher)).Methods("DELETE")
 
 }
